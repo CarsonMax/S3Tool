34511c755c20464d22b25e8e05649174fa5ef3cd
CarsonMax <luzhucaine@outlook.com>
2020-09-21 17:47:19 +0800

    init

---
+++ S3Tool/Data/EFCoreDbContext.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using S3Tool.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace S3Tool.Data
+{
+
+    /// <summary>
+    /// 数据上下文类，继承自DbContext
+    /// </summary>
+    public class EFCoreDbContext : DbContext
+    {
+        /// <summary>
+        /// 重写OnConfiguring方法
+        /// </summary>
+        /// <param name="optionsBuilder"></param>
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // 使用SqlServer数据库，传递连接字符串
+            optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=EFTestDb;User ID=sa;Password=sa1234;");
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        /// <summary>
+        /// 重写OnModelCreating，主要做一些配置
+        /// 例如设置生成的表名、主键、字符长度
+        /// </summary>
+        /// <param name="modelBuilder"></param>
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // 设置生成的表名
+            modelBuilder.Entity<Student>().ToTable("T_Student");
+            // 设置主键，可以不设置，会默认把Id字段当成主键
+            modelBuilder.Entity<Student>().HasKey(p => p.Id);
+            // 设置Name字段的最大长度
+            modelBuilder.Entity<Student>().Property("Name").HasMaxLength(32);
+            base.OnModelCreating(modelBuilder);
+        }
+
+        // DbSet属性
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ S3Tool/Program.cs
@@ -0,0 +1,26 @@
+﻿using S3Tool.Data;
+using System;
+
+namespace S3Tool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            EFCoreDbContext dbContext = new EFCoreDbContext();
+            //bool tfTrue = dbContext.Database.EnsureCreated();
+            //if (tfTrue)
+            //{
+            //    Console.WriteLine("数据库创建成功!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("数据库创建失败!");
+            //}
+
+            Console.ReadKey();
+        }
+    }
+}
